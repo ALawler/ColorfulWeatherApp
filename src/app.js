ae8486c1fcd40eb3850872e7ac9bf0e1ffc56501
ALawler <AEGLawler@gmail.com>
2023-02-03 14:47:35 -0500

    Updated API
    apiURL fixed and restored link to API

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ function searchCity(query) {
   let apiEndpoint = "https://api.shecodes.io/weather/v1/current";
   let key = "0bbef54a49efc7of4df96ea8t63e36a3";
   let units = "imperial";
-  let apiUrl = `${apiEndpoint}?q=${query}&key=${key}&units=${units}`;
+  let apiUrl = `${apiEndpoint}?query=${query}&key=${key}&units=${units}`;
 
   axios.get(apiUrl).then(console.log(apiUrl));
 }
 
-searchCity("Detroit");
+searchCity("Seattle");
