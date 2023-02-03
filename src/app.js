821c3b9c930234a4ea705a32f7d5ae4385115996
ALawler <AEGLawler@gmail.com>
2023-02-03 15:14:57 -0500

    API logged Main Weather Info/No graphics

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+function displayWeather(response) {
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#place").innerHTML = response.data.city;
+  document.querySelector("#temp").innerHTML = `${Math.round(
+    response.data.temperature.current
+  )} °F`;
+  document.querySelector("#humidity").innerHTML = `${Math.round(
+    response.data.temperature.humidity
+  )}`;
+  document.querySelector("#windSpeed").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )}`;
+}
+
 function searchCity(query) {
   let apiEndpoint = "https://api.shecodes.io/weather/v1/current";
   let key = "0bbef54a49efc7of4df96ea8t63e36a3";
   let units = "imperial";
   let apiUrl = `${apiEndpoint}?query=${query}&key=${key}&units=${units}`;
 
-  axios.get(apiUrl).then(console.log(apiUrl));
+  axios.get(apiUrl).then(displayWeather);
 }
 
 searchCity("Seattle");
